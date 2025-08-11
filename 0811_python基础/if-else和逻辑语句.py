04522c74a0edc88b3918ed37b8d385e13b0b3a67
luxu0628 <2680532813@qq.com>
2025-08-11 14:35:41 +0800

    first commit

---
+++ 0811_python基础/if-else和逻辑语句.py
@@ -0,0 +1,6 @@
+name=input("你是谁呀？")
+host=input("你是谁的呀？")
+if name=="我是小傀儡" and host=="姝雅的":
+        print("快进来吧，姝雅的小傀儡，好可爱一只")
+else:
+    print("我不认识你，你不是姝雅的小傀儡")
---
+++ 0811_python基础/反转字符串.py
@@ -0,0 +1,4 @@
+inputwords=input("输入你要反转的字符串").split()
+inputwords=inputwords[::-1]
+out="".join(inputwords)
+print(out)
